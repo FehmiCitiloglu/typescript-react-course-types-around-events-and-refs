7a3c2ea9f842e9def70e1887bbdde27d2bd3ef12
FehmiCitiloglu <fehmicitiloglu@gmail.com>
2021-10-23 19:05:47 +0300

    last commit

---
+++ src/props/Child.tsx
@@ -0,0 +1,29 @@
+interface ChildProps {
+  color: string;
+  onClick: () => void;
+}
+
+const Child = ({ color, onClick }: ChildProps) => {
+  return (
+    <div>
+      {color}
+      <button onClick={onClick}>Click me</button>
+    </div>
+  );
+};
+
+export default Child;
+
+export const ChildAsFC: React.FC<ChildProps> = ({
+  color,
+  onClick,
+  children,
+}) => {
+  return (
+    <div>
+      {color}
+      {children}
+      <button onClick={onClick}>Click me</button>
+    </div>
+  );
+};
